5d3791c210b8f99a1ae53a51dfd05502af46d29f
deonokongo <lilian.deon29@gmail.com>
2024-07-22 23:00:29 -0700

    added missing closing braces

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -6,3 +6,4 @@ export default function iterateThroughObject(reportWithIterator) {
   }
 
   return employees.join(' | ');
+}
--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -5,3 +5,4 @@ export default function createReportObject(employeesList) {
       return Object.keys(employeesList).length;
     },
   };
+}
